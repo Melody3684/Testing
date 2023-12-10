09c40378e105a96c8ccfd48317e7f30dbff13476
Melody3684 <153463612+Melody3684@users.noreply.github.com>
2023-12-10 13:54:32 -0800

    Add files via upload

---
+++ Testing.py
@@ -0,0 +1,7 @@
+print("Hi!! Pick a day between 1 and 10.")
+day = int(input()) #the int is casting to make the string (the input) a number
+print(day)
+if ((day == 5) or (day == 4) or (day == 3)):
+        print("I am on vacation and I miss you!!")
+else:
+        print("I am at home and I am bored.")
