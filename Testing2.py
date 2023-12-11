05a7130c86e76cde29340e143c04f4ca4cbdb10a
Melody3684 <153463612+Melody3684@users.noreply.github.com>
2023-12-10 16:24:09 -0800

    Add files via upload

---
+++ Testing2.py
@@ -0,0 +1,10 @@
+room_cost = 300
+tax = room_cost * .1
+total = room_cost + tax
+guest = 6
+cost_per_person = total / guest
+print("Room cost is: " + str(room_cost))
+print("tax is: " + str(tax))
+print("Tatal is: " + str(total))
+print("There are this many guest: " + str(guest) )
+print("The cost per person is: " + str(cost_per_person))
